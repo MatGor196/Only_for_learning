dc7304147656395ddbf9817b492236aa15ea54cf
Mateusz Goraj <gorajmateusz1@gmail.com>
2025-01-06 17:27:57 +0100

    Dzień 4

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
 ﻿
-Console.WriteLine("Hello, World!");
+var name = "Adam";
+var sex = true; //true - mężczyzna, false - kobieta
+var age = 30;
+
+if(sex == false && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+if(sex == true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
