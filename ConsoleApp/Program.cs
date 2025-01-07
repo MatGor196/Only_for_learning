aba5d2ca07751e4e6fc25ceadf120b72d287362d
Mateusz Goraj <gorajmateusz1@gmail.com>
2025-01-07 21:32:10 +0100

    Dzień 5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,17 +1,57 @@
 ﻿
-var name = "Adam";
-var sex = true; //true - mężczyzna, false - kobieta
-var age = 30;
+int number = 4566;
 
-if(sex == false && age < 30)
+string number_in_string = number.ToString();
+char[] digits = number_in_string.ToCharArray();
+
+Console.WriteLine(number);
+int[] digits_in_number = {0,0,0,0,0,0,0,0,0,0};
+
+foreach (char digit in digits)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (digit == '0')
+    {
+        digits_in_number[0]++;
+    }
+    else if (digit == '1')
+    {
+        digits_in_number[1]++;
+    }
+    else if (digit == '2')
+    {
+        digits_in_number[2]++;
+    }
+    else if (digit == '3')
+    {
+        digits_in_number[3]++;
+    }
+    else if (digit == '4')
+    {
+        digits_in_number[4]++;
+    }
+    else if (digit == '5')
+    {
+        digits_in_number[5]++;
+    }
+    else if (digit == '6')
+    {
+        digits_in_number[6]++;
+    }
+    else if (digit == '7')
+    {
+        digits_in_number[7]++;
+    }
+    else if (digit == '8')
+    {
+        digits_in_number[8]++;
+    }
+    else if (digit == '9')
+    {
+        digits_in_number[9]++;
+    }
 }
-else if(name == "Ewa" && age == 30)
+
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Ewa, lat 30");
-}
-else if(sex == true && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine(i + " => " + digits_in_number[i]);
 }
