5a6c9af080593d822da9e8e30b8be6b66e48885a
Mateusz Goraj <gorajmateusz1@gmail.com>
2025-01-08 22:24:51 +0100

    Dzień 6

---
+++ ConsoleApp/Employee.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp
+{
+    class Employee
+    {
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+
+        private List<int> marks;
+        public Employee(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+
+            this.marks = new List<int>();
+        }
+
+        public void AddMark(int mark_to_add)
+        {
+            if (mark_to_add >= 1 && mark_to_add <= 10)
+            {
+                this.marks.Add(mark_to_add);
+            }
+        }
+
+        public int Result()
+        {
+            if (this.marks.Count == 0)
+                return 0;
+
+            int sum = 0;
+
+            foreach (var mark in this.marks)
+            {
+                sum += mark;
+            }
+
+            return sum;
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,57 +1,43 @@
-﻿
-int number = 4566;
+﻿using ConsoleApp;
 
-string number_in_string = number.ToString();
-char[] digits = number_in_string.ToCharArray();
+var employee1 = new Employee("Adam", "Kowalski", 30);
+employee1.AddMark(1);
+employee1.AddMark(2);
+employee1.AddMark(3);
+employee1.AddMark(4);
+employee1.AddMark(5);
 
-Console.WriteLine(number);
-int[] digits_in_number = {0,0,0,0,0,0,0,0,0,0};
+var employee2 = new Employee("Kasia", "Nowak", 20);
+employee2.AddMark(1);
+employee2.AddMark(4);
+employee2.AddMark(3);
+employee2.AddMark(7);
+employee2.AddMark(8);
 
-foreach (char digit in digits)
+var employee3 = new Employee("Krystian", "Żukowski", 25);
+employee3.AddMark(3);
+employee3.AddMark(5);
+employee3.AddMark(7);
+employee3.AddMark(5);
+employee3.AddMark(8);
+
+var ListOfEmployees = new List<Employee>()
+    {employee1, employee2, employee3};
+
+int result = -1;
+var employee_with_max_value = new Employee("---", "---", 0);
+
+foreach (var employee in ListOfEmployees)
 {
-    if (digit == '0')
+    var current_result = employee.Result();
+    if (current_result > result)
     {
-        digits_in_number[0]++;
-    }
-    else if (digit == '1')
-    {
-        digits_in_number[1]++;
-    }
-    else if (digit == '2')
-    {
-        digits_in_number[2]++;
-    }
-    else if (digit == '3')
-    {
-        digits_in_number[3]++;
-    }
-    else if (digit == '4')
-    {
-        digits_in_number[4]++;
-    }
-    else if (digit == '5')
-    {
-        digits_in_number[5]++;
-    }
-    else if (digit == '6')
-    {
-        digits_in_number[6]++;
-    }
-    else if (digit == '7')
-    {
-        digits_in_number[7]++;
-    }
-    else if (digit == '8')
-    {
-        digits_in_number[8]++;
-    }
-    else if (digit == '9')
-    {
-        digits_in_number[9]++;
+        employee_with_max_value = employee;
+        result = current_result;
     }
 }
 
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine(i + " => " + digits_in_number[i]);
-}
+Console.WriteLine("Pracownik z najlepszymi ocenami:");
+Console.WriteLine(employee_with_max_value.name + " " +
+                  employee_with_max_value.surname + ", " +
+                  employee_with_max_value.age);
